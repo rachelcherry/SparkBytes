b0b102a27992fea272d658ae3970d21be70ddd30
amelia-liston <aliston@bu.edu>
2024-04-04 18:03:28 -0400

    working on 4 requirements of Frontent Login Screen. About to begin working on implementing the Antd componenets

--- client/src/pages/login/index.tsx
+++ client/src/pages/login/index.tsx
@@ -1,6 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import { Form, Input, Button, message } from "antd";
+import { useAuth } from ".../AuthContext";
 
 const Login = () => {
+  const [loading, setLoading] = useState(false);
+  const { login } = useAuth();
+
+  const onComplete = async (values) => {
+    setLoading(true);
+    try {
+      const response = await fetch("/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(values),
+      });
+      const data = await response.json();
+
+      if (response.ok) {
+        //we want to store the token into AuthToken if the login is successful
+        login(data.token);
+        message.success("Login successful");
+      } else {
+        //displays an error message when there are invalid credentials
+        message.error(data.message);
+      }
+    } catch (error) {
+      console.error("Login failed: ", error);
+      message.error("An error has ocurred, please try again later.");
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return <div>Login</div>;
 };
 
