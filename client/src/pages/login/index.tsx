77d74772aa42883c90747ce76201cbdd8b6d21e1
amelia-liston <aliston@bu.edu>
2024-04-04 22:43:37 -0400

    Completed styling Login page. Also fixed previous issue where I wasn't storing the token into AuthContext

--- client/src/pages/login/index.tsx
+++ client/src/pages/login/index.tsx
@@ -1,12 +1,17 @@
 import React, { useState } from "react";
 import { Form, Input, Button, message } from "antd";
-import { useAuth } from ".../AuthContext";
+import { useAuth } from "@/contexts/AuthContext";
+import { UserOutlined, LockOutlined } from "@ant-design/icons";
 
 const Login = () => {
   const [loading, setLoading] = useState(false);
-  const { login } = useAuth();
+  const { updateAuthToken } = useAuth(); //getting the updateAuthToken from useAuth
 
-  const onComplete = async (values) => {
+  const login = async (token: string) => {
+    updateAuthToken(token); //calls updateAuthToken function to update authentication token
+  };
+
+  const onFinish = async (values) => {
     setLoading(true);
     try {
       const response = await fetch("/login", {
@@ -20,7 +25,7 @@ const Login = () => {
 
       if (response.ok) {
         //we want to store the token into AuthToken if the login is successful
-        login(data.token);
+        login(data.token); //calls login function to store the token
         message.success("Login successful");
       } else {
         //displays an error message when there are invalid credentials
@@ -34,7 +39,36 @@ const Login = () => {
     }
   };
 
-  return <div>Login</div>;
+  return (
+    <div style={{ maxWidth: "400px", margin: "0 auto" }}>
+      <h1 style={{ alignContent: "center" }}>Login</h1>
+      <Form name="login" onFinish={onFinish}>
+        <Form.Item
+          name="name"
+          rules={[
+            { required: true, message: "Please input your email" },
+            { type: "email", message: "Invalid email format" },
+          ]}
+        >
+          <Input prefix={<UserOutlined />} placeholder="Email"></Input>
+        </Form.Item>
+        <Form.Item
+          name="password"
+          rules={[{ required: true, message: "Please input your password" }]}
+        >
+          <Input.Password
+            prefix={<LockOutlined />}
+            placeholder="Password"
+          ></Input.Password>
+        </Form.Item>
+        <Form.Item>
+          <Button type="primary" htmlType="submit" loading={loading}>
+            Login
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  );
 };
 
 export default Login;
