d02dc24b56c317f922964f5d351507bdf9a22e58
amelia-liston <aliston@bu.edu>
2024-04-17 22:58:56 -0400

    styling [eventId].tsx using styling for events page

--- client/src/pages/events/[eventId].tsx
+++ client/src/pages/events/[eventId].tsx
@@ -2,10 +2,23 @@ import { useEffect, useState, useContext } from "react";
 import { useRouter } from "next/router";
 import { API_URL } from "@/common/constants";
 import { IEvent } from "@/common/interfaces_zod";
-import { Card, Typography } from "antd";
+import {
+  List,
+  Card,
+  Pagination,
+  message,
+  Typography,
+  Select,
+  Button,
+  Tag,
+} from "antd";
 import { AuthContext } from "@/contexts/AuthContext";
+import { IAuthTokenDecoded, ITag } from "../../common/interfaces";
+import { FilterOutlined, UserOutlined } from "@ant-design/icons";
+import dayjs from "dayjs"; //for formatting the times
 
 const { Title, Paragraph } = Typography;
+const { Option } = Select;
 
 const EventDetails = () => {
   //get event_id from router
@@ -60,10 +73,81 @@ const EventDetails = () => {
   }
   //finally render the event details once the event has loaded
   return (
-    <div>
-      <Title level={2}>{event.event_id}</Title>
-      <Card>
-        <Paragraph>Description: {event.description}</Paragraph>
+    <div
+      style={{
+        backgroundColor: "#eaf7f0",
+        padding: "20px",
+        width: "100%",
+        height: "100%",
+      }}
+    >
+      <Typography.Title
+        level={2}
+        style={{ textAlign: "center", marginBottom: "20px" }}
+      >
+        {"Event "}
+        {event.event_id}
+      </Typography.Title>
+      <Card
+        title={`Event ID: ${event.event_id}`}
+        style={{
+          backgroundColor: "white",
+          borderRadius: "0.625rem",
+          boxShadow: "0 2px 6px rgba(0, 0, 0, 0.1)",
+        }}
+      >
+        {event.createdById == authState?.decodedToken?.id && (
+          <div
+            style={{
+              position: "absolute",
+              top: "18px",
+              right: "20px",
+              zIndex: 1,
+            }}
+          >
+            <Tag
+              color="#66BB6A"
+              style={{
+                borderRadius: "0.625rem",
+                boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
+                padding: "1px 6px",
+                fontSize: "12px",
+              }}
+            >
+              <UserOutlined style={{ marginRight: "4px", fontSize: "12px" }} />
+              My Event
+            </Tag>
+          </div>
+        )}
+        <Paragraph
+          style={{
+            color: "#66BB6A",
+            fontSize: "12px",
+            marginBottom: "6px",
+          }}
+        >
+          Post Time: {dayjs(event.post_time).format("YYYY-MM-DD HH:mm")} <br />
+          Expire Time: {dayjs(event.exp_time).format("YYYY-MM-DD HH:mm")}
+        </Paragraph>
+        <Paragraph style={{ color: "black", lineHeight: "3" }}>
+          {/* Created by: {event.createdBy.name} <br /> */}
+          Description: {event.description} <br />
+          Quantity: {event.qty} <br />
+          {/* Tags:{" "}
+          {event.tags && event.tags.length > 0
+            ? event.tags.map((tag, index) => (
+                <span key={(tag as ITag).tag_id}>
+                  {(tag as ITag).name}
+                  {index !== event.tags.length - 1 && ", "}
+                </span>
+              ))
+            : " Not specified"} */}
+          {/* <br /> */}
+          {/* Location:{" "} */}
+          {/* {event.location
+            ? `${event.location.Address}, Floor ${event.location.floor}, Room ${event.location.room}`
+            : "Not specified"} */}
+        </Paragraph>
       </Card>
     </div>
   );
