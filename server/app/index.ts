f862d2a26a0a3b0699135803e3886b83cb061a5e
mmyman <56144148+mmyman@users.noreply.github.com>
2024-04-23 22:56:45 -0400

    Revert "tried adding deployed site to cors directly"
    This reverts commit e50e07e0bf8a0a467948ac64c0f2c16efd19ec9e.

--- server/app/index.ts
+++ server/app/index.ts
@@ -11,9 +11,7 @@ const app = express();
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
-app.use(cors({
-    origin: 'https://spark-bytes-project-team1-production.up.railway.app/'
-}));
+app.use(cors());
 app.use(express.json());
 
 app.use('/api/', helloRouter);
