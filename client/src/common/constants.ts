362aa87ba03f5325c172667f1d39e9077bca3705
mmyman <56144148+mmyman@users.noreply.github.com>
2024-04-24 12:44:20 -0400

    added https to production switch

--- client/src/common/constants.ts
+++ client/src/common/constants.ts
@@ -1,4 +1,4 @@
 export const API_URL: string =
   process.env.NODE_ENV === "production"
-    ? "backend-production-316c.up.railway.app"
+    ? "http://backend-production-316c.up.railway.app"
     : "http://localhost:5005";
