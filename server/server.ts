d3b2cd7b736b4d9470c994f809ce3a980fdaaf4e
mmyman <56144148+mmyman@users.noreply.github.com>
2024-04-24 12:34:13 -0400

    print error message when starting server server doesn't find port

--- server/server.ts
+++ server/server.ts
@@ -2,5 +2,5 @@ import app from './app/index.ts';
 const port = process.env.PORT || '5005';
 app.listen(port, (err?: any) => {
   if (err) throw err;
-  console.log('> Ready on http://localhost:5005');
+  console.log('> Ready on http://localhost:5005' + err);
 });
