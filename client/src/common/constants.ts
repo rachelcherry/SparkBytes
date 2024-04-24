34d7d0f55432f337ed48ef3af8ce1eb5e9ea3180
Owen Mariani <mariani.owen@gmail.com>
2024-04-24 11:47:37 -0400

    changed production switch to be backend url

--- client/src/common/constants.ts
+++ client/src/common/constants.ts
@@ -1,4 +1,4 @@
 export const API_URL: string =
   process.env.NODE_ENV === "production"
-    ? "https://spark-bytes-project-team1-production.up.railway.app"
+    ? "backend-production-316c.up.railway.app"
     : "http://localhost:5005";
