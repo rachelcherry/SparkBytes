9c8e0600440b3500ba02ee4e72d541676f6afa05
rachelcherry <rcherry@bu.edu>
2024-05-02 11:08:07 -0400

    adding owen's changes back

--- client/src/pages/events/index.tsx
+++ client/src/pages/events/index.tsx
@@ -10,11 +10,13 @@ import {
   Button,
   Tag,
 } from "antd";
+import { EventCard } from "@/components/Events/EventCard";
 import { API_URL } from "../../common/constants";
 import { IAuthTokenDecoded, IEvent, ITag } from "../../common/interfaces";
 import { FilterOutlined, UserOutlined } from "@ant-design/icons";
 import dayjs from "dayjs";
 import { useAuth } from "@/contexts/AuthContext";
+import { useFetch } from "@/utility/useFetch";
 const { Paragraph } = Typography;
 const { Option } = Select;
 
@@ -23,47 +25,36 @@ interface ITokenState {
   decodedToken: IAuthTokenDecoded;
 }
 const Events: FC = () => {
-  const [isLoading, setIsLoading] = useState<boolean>(true);
   const [events, setEvents] = useState<IEvent[]>([]);
-  const [selectedEvent, setSelectedEvent] = useState<IEvent | null>(null);
   const [currentPage, setCurrentPage] = useState<number>(1);
   const [pageSize, setPageSize] = useState<number>(10);
-  const [totalEvents, setTotalEvents] = useState<number>(0);
   const [filteredTag, setFilteredTag] = useState<string | null>(null);
   const [isFilterVisible, setIsFilterVisible] = useState(false);
   const [sortDesc, setSortDesc] = useState<boolean>(false);
 
   const router = useRouter();
   const { getAuthState, authState } = useAuth();
+
+  const {
+    data: eventsData,
+    isLoading,
+    error,
+  } = useFetch<{ events: IEvent[] }>(`${API_URL}/api/events/`, {
+    headers: {
+      Authorization: `Bearer ${getAuthState()?.token}`,
+    },
+  });
   useEffect(() => {
-    const fetchEvents = async () => {
-      try {
-        setIsLoading(true);
-        const eventsResponse = await fetch(`${API_URL}/api/events/`, {
-          headers: {
-            Authorization: `Bearer ${getAuthState()?.token}`,
-          },
-        });
-
-        if (!eventsResponse.ok) {
-          console.log("error");
-          throw new Error(`${eventsResponse.status}`);
-        }
-
-        const responseJSON = await eventsResponse.json();
-        const eventsData = responseJSON.events;
-        setEvents(eventsData);
-        setIsLoading(false);
-        setTotalEvents(eventsData.length);
-      } catch (error) {
-        console.error(error);
-        message.error(
-          "An error occurred while fetching events. Please try again later."
-        );
-      }
-    };
-    fetchEvents();
-  }, []);
+    if (error) {
+      message.error(
+        "An error occurred while fetching events. Please try again later."
+      );
+      console.error(error);
+    }
+    if (eventsData) {
+      setEvents(eventsData.events);
+    }
+  }, [error, eventsData]);
 
   const handlePageChange = (page: number, pageSize?: number) => {
     setCurrentPage(page);
@@ -97,7 +88,6 @@ const Events: FC = () => {
   };
 
   const handleEventClick = (event: IEvent) => {
-    setSelectedEvent(event);
     router.push(`/events/${event.event_id}`); // Navigate to the view page
   };
 
@@ -182,71 +172,7 @@ const Events: FC = () => {
         loading={isLoading}
         renderItem={(event: IEvent) => (
           <List.Item>
-            <Card
-              onClick={() => handleEventClick(event)}
-              title={`Event ID: ${event.event_id}`}
-              style={{
-                backgroundColor: "white",
-                borderRadius: "0.625rem",
-                boxShadow: "0 2px 6px rgba(0, 0, 0, 0.1)",
-              }}
-            >
-              {event.createdById == authState?.decodedToken?.id && (
-                <div
-                  style={{
-                    position: "absolute",
-                    top: "18px",
-                    right: "20px",
-                    zIndex: 1,
-                  }}
-                >
-                  <Tag
-                    color="#66BB6A"
-                    style={{
-                      borderRadius: "0.625rem",
-                      boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
-                      padding: "1px 6px",
-                      fontSize: "12px",
-                    }}
-                  >
-                    <UserOutlined
-                      style={{ marginRight: "4px", fontSize: "12px" }}
-                    />
-                    My Event
-                  </Tag>
-                </div>
-              )}
-              <Paragraph
-                style={{
-                  color: "#66BB6A",
-                  fontSize: "12px",
-                  marginBottom: "6px",
-                }}
-              >
-                Post Time: {dayjs(event.post_time).format("YYYY-MM-DD HH:mm")}{" "}
-                <br />
-                Expire Time: {dayjs(event.exp_time).format("YYYY-MM-DD HH:mm")}
-              </Paragraph>
-              <Paragraph style={{ color: "black", lineHeight: "3" }}>
-                Created by: {event.createdBy.name} <br />
-                Description: {event.description} <br />
-                Quantity: {event.qty} <br />
-                Tags:{" "}
-                {event.tags && event.tags.length > 0
-                  ? event.tags.map((tag, index) => (
-                      <span key={(tag as ITag).tag_id}>
-                        {(tag as ITag).name}
-                        {index !== event.tags.length - 1 && ", "}
-                      </span>
-                    ))
-                  : " Not specified"}
-                <br />
-                Location:{" "}
-                {event.location
-                  ? `${event.location.Address}, Floor ${event.location.floor}, Room ${event.location.room}`
-                  : "Not specified"}
-              </Paragraph>
-            </Card>
+            <EventCard event={event} handleEventClick={handleEventClick} />
           </List.Item>
         )}
       />
@@ -262,7 +188,7 @@ const Events: FC = () => {
         <Pagination
           current={currentPage}
           pageSize={pageSize}
-          total={totalEvents}
+          total={events?.length || 0}
           onChange={handlePageChange}
           style={{ marginTop: "20px" }}
         />
